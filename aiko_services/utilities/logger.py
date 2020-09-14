f265407eb90d9b7eb8a576298851db06f7730a93
Jon Oxer <jon@lounge.modem>
2020-09-14 21:06:36 +1000

    Prevent superfluous "transitions.core" debug log messages.

--- aiko_services/utilities/logger.py
+++ aiko_services/utilities/logger.py
@@ -41,6 +41,9 @@ _CONFIGURATION = {
         "STATE": { 
             "handlers": ["h"],
             "level": "INFO"
+        },
+        "transitions.core": { 
+            "level": "ERROR"
         }
     }
 }
