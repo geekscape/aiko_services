geekscape/aiko_services

commit 62710840eb1b56cc59a4e2ce20b9183aa381d26c

Update to pipeline with no state machine, use yaml.FullLoader

aiko_services/pipeline.py
@@ -251,7 +251,7 @@ def load_pipeline_definition(pipeline_pathname, pipeline_name=PIPELINE_DEFINITIO
             pipeline_def = json.load(f)[pipeline_name]
     elif pipeline_pathname.endswith(".yaml") or pipeline_pathname.endswith(".yml"):
         with open(pipeline_pathname, "r") as f:
-            pipeline_def = yaml.load(f, Loader=yaml.SafeLoader)[pipeline_name]
+            pipeline_def = yaml.load(f, Loader=yaml.FullLoader)[pipeline_name]
     else:
         raise ValueError(f"Unsupported pipeline definition format: {pipeline_pathname}")
 

examples/pipeline/video_example.yaml
@@ -1,43 +1,0 @@
-pipeline_definition:
-- module: aiko_services.elements.video_io
-  name: VideoReadFile
-  parameters:
-    video_pathname: astra_brief.mp4
-  successors:
-    alternate:
-    - ImageAnnotate2
-    default:
-    - ImageAnnotate1
-- module: aiko_services.elements.image_io
-  name: ImageAnnotate1
-  successors:
-  - ImageOverlay
-- module: aiko_services.elements.image_io
-  name: ImageAnnotate2
-  successors:
-  - ImageOverlay
-- module: aiko_services.elements.image_io
-  name: ImageOverlay
-  successors:
-  - VideoShow
-- module: aiko_services.elements.video_io
-  name: VideoShow
-  parameters:
-    window_location:
-    - 50
-    - 50
-    window_title: Astra
-  successors:
-  - ImageResize
-- module: aiko_services.elements.image_io
-  name: ImageResize
-  parameters:
-    new_height: 360
-    new_width: 640
-  successors:
-  - VideoWriteFile
-- module: aiko_services.elements.video_io
-  name: VideoWriteFile
-  parameters:
-    video_frame_rate: 29.97
-    video_pathname: z_output.mp4

examples/pipeline/video_to_images.py
@@ -38,5 +38,13 @@ pipeline_definition = [
     }
 ]
 
+import json
+import yaml
+with open("video_to_images.json", 'w') as f:
+    json.dump({"pipeline_definition": pipeline_definition}, f, indent=2)
+
+with open("video_to_images.yaml", 'w') as f:
+    yaml.dump({"pipeline_definition": pipeline_definition}, f)
+
 if __name__ == "__main__":
     Pipeline(pipeline_definition, FRAME_RATE).run()

examples/pipeline/video_to_images.yaml
@@ -1,0 +1,30 @@
+pipeline_definition:
+- module: aiko_services.elements.video_io
+  name: VideoReadFile
+  parameters:
+    video_pathname: astra_brief.mp4
+  successors:
+  - ImageOverlay
+- module: aiko_services.elements.image_io
+  name: ImageOverlay
+  parameters:
+    colors:
+      astra: !!python/tuple
+      - 100
+      - 0
+      - 0
+      bailey: !!python/tuple
+      - 0
+      - 100
+      - 0
+      ty: !!python/tuple
+      - 0
+      - 0
+      - 100
+    text_color: yellow
+  successors:
+  - ImageWriteFile
+- module: aiko_services.elements.image_io
+  name: ImageWriteFile
+  parameters:
+    image_pathname: z_output/image_{:06d}.jpg

commit 2ae73fe3d341686f27a57cb26b0957f73690da1b

Fix "aiko_services/pipeline.py": Put back required support for Pipeline Declarations that define a StateMachine.  Was broken by commit 62710840eb1b56cc59a4e2ce20b9183aa381d26c

.gitignore
@@ -127,3 +127,5 @@ dmypy.json
 
 # Pyre type checker
 .pyre/
+
+z_*

aiko_services/cli.py
@@ -1,6 +1,14 @@
+#!/usr/bin/env python3
+#
+# Aiko Service: Command Line Interface
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
 # Usage
 # ~~~~~
+# aiko <pipeline_definition_pathname> [--help]
 #
+# Description
+# ~~~~~~~~~~~
 # The Pipeline CLI is designed to slip ontop of existing 
 # pipeline definitions. I learn best with an exaple:
 # 

aiko_services/message/mqtt.py
@@ -1,15 +1,21 @@
 # To Do
 # ~~~~~
-# - BUG: If on MQTT thread, then if waiting for a condition to change that depends upon an incoming
-#        message, then wait will timeout, because that incoming message can't be processed.
-#        For example, if wait_disconnected() called whilst on MQTT thread, then the MQTT broker
-#        message for "disconnect" --> on_disconnect() won't happen.
-#        For example, when Registrar processed "(primary stopped)" message and attempts
-#        set_last_will_and_testament(), which causes a wait_disconnected() whilst on the MQTT thread.
-# - SOLUTION: By default queue all incoming MQTT messages and process on the main event.loop() !
+# - BUG: If on MQTT thread, then if waiting for a condition to change that
+#        depends upon an incoming message, then wait will timeout, because
+#        that incoming message can't be processed.
+#        For example, if wait_disconnected() called whilst on MQTT thread,
+#        then the MQTT broker message for "disconnect" --> on_disconnect()
+#        won't happen.
+#        For example, when Registrar processed "(primary stopped)" message
+#        and attempts set_last_will_and_testament(), which causes a
+#        wait_disconnected() whilst on the MQTT thread.
+# - SOLUTION: By default queue all incoming MQTT messages and process on the
+#        main event.loop() !
 #
-# - Refactor wait_disconnected(), wait_connected() and wait_published(self) into a single function
-#   - Parameter is a variable condition to test, e.g not _CONNECTED, _CONNECTED and _PUBLISHED
+# - Refactor wait_disconnected(), wait_connected() and wait_published(self)
+#     into a single function
+#   - Parameter is a variable condition to test,
+#       e.g not _CONNECTED, _CONNECTED and _PUBLISHED
 #
 # - Implement message to change logging level !
 # - Allow default MQTT_HOST and MQTT_PORT to be overridden by CLI parameters
@@ -33,8 +39,8 @@ __all__ = ["MQTT"]
 
 # MQTT_HOST = "mqtt.fluux.io"
 # MQTT_HOST = "test.mosquitto.org"
-# MQTT_HOST = "localhost"
-MQTT_HOST = "lounge.local"
+MQTT_HOST = "localhost"
+# MQTT_HOST = "lounge.local"
 MQTT_PORT = 1883
 
 _CONNECTED = False

aiko_services/pipeline.py
@@ -14,10 +14,10 @@
 # - Add Pipeline.state ...
 #   - When pipeline stopped ... event.remove_timer_handler(timer_test)
 
+import json
 import networkx as nx
 from queue import Queue
 import time
-import json
 import yaml
 
 import aiko_services.event as event
@@ -140,6 +140,8 @@ class Pipeline():
                 try:
                     node_name, parameter_name = name.split(":")
                     self.update_node_parameter(node_name, parameter_name, parameter_value)
+                except KeyError:
+                    _LOGGER.error(f"pipeline_handler(): Invalid Pipeline Element: {node_name}")
                 except ValueError:
                     _LOGGER.error(f"pipeline_handler(): Invalid parameter name: {name}")
             return
@@ -242,17 +244,22 @@ class Pipeline():
 
 PIPELINE_DEFINITION_NAME = "pipeline_definition"
 
-def load_pipeline_definition(pipeline_pathname, pipeline_name=PIPELINE_DEFINITION_NAME):
+def load_pipeline_definition(pipeline_pathname, pipeline_definition_name=PIPELINE_DEFINITION_NAME):
+    state_machine_model = None
     if pipeline_pathname.endswith(".py"):
-        mod = load_module(pipeline_pathname)
-        pipeline_def =  getattr(mod, pipeline_name)
+        module = load_module(pipeline_pathname)
+        pipeline_definition =  getattr(module, pipeline_definition_name)
+        try:
+            state_machine_model = getattr(module, "StateMachineModel")
+        except AttributeError:
+            pass
     elif pipeline_pathname.endswith(".json"):
-        with open(pipeline_pathname, "r") as f:
-            pipeline_def = json.load(f)[pipeline_name]
+        with open(pipeline_pathname, "r") as file:
+            pipeline_definition = json.load(file)[pipeline_definition_name]
     elif pipeline_pathname.endswith(".yaml") or pipeline_pathname.endswith(".yml"):
-        with open(pipeline_pathname, "r") as f:
-            pipeline_def = yaml.load(f, Loader=yaml.FullLoader)[pipeline_name]
+        with open(pipeline_pathname, "r") as file:
+            pipeline_definition = yaml.load(file, Loader=yaml.FullLoader)[pipeline_definition_name]
     else:
         raise ValueError(f"Unsupported pipeline definition format: {pipeline_pathname}")
 
-    return pipeline_def
+    return pipeline_definition, state_machine_model

aiko_services/utilities/configuration.py
@@ -1,5 +1,6 @@
 # To Do
 # ~~~~~
+# - Implement discovery for finding the default MQTT hostname
 # - Implement discovery for finding the default namespace
 #   - Define a NameSpace class for holding information about a specific namespace
 