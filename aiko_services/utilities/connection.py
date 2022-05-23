6283cc24285bffd1e2560671d1543dfae661a5ca
Andy Gelme <geekscape@gmail.com>
2022-05-23 18:09:27 +1000

    Initial commit for "aiko_services/utilities/connection.py" with ConnectionState and Connection classes.

---
+++ aiko_services/utilities/connection.py
@@ -0,0 +1,47 @@
+# Notes
+# ~~~~~
+# This is a low-level mechanism, used by the AikoServices framework
+# and should not have any static dependencies on that framework
+#
+# To Do
+# ~~~~~
+# - Add support for ConnectionState.TRANSPORT (MQTT) connection state updates
+
+import logging
+
+__all__ = ["ConnectionState", "Connection"]
+
+class ConnectionState:
+    NONE = "NONE"
+    NETWORK = "NETWORK"
+    TRANSPORT = "TRANSPORT"  # MQTT, Ray, ROS2, etc
+    REGISTRAR = "REGISTRAR"
+
+    states = [NONE, NETWORK, TRANSPORT, REGISTRAR]  # order matters
+
+    @classmethod
+    def index(cls, connection_state):  # throws ValueError
+        return cls.states.index(connection_state)
+
+class Connection:
+    def __init__(self):
+        self.connection_state = ConnectionState.NONE
+        self.connection_state_handlers = []
+
+    def add_handler(self, connection_state_handler):
+        connection_state_handler(self, self.connection_state)
+        if not connection_state_handler in self.connection_state_handlers:
+            self.connection_state_handlers.append(connection_state_handler)
+
+    def is_connected(self, connection_state):  # throws ValueError
+        current_state_index = ConnectionState.index(self.connection_state)
+        return current_state_index >= ConnectionState.index(connection_state)
+
+    def remove_handler(self, connection_state_handler):
+        if connection_state_handler in self.connection_state_handlers:
+           self.connection_state_handlers.remove(connection_state_handler)
+
+    def update_state(self, connection_state):
+        self.connection_state = connection_state
+        for connection_state_handler in self.connection_state_handlers:
+            connection_state_handler(self, connection_state)
