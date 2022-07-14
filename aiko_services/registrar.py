9c48901ff3c19e70401252653a167676f10942cb
Andy Gelme <geekscape@gmail.com>
2022-07-15 05:03:20 +1000

    Improve "aiko_services/registrar.py" to be a proper Service.

--- aiko_services/registrar.py
+++ aiko_services/registrar.py
@@ -66,7 +66,6 @@
 #     - Find the primary registrar (if it exists ?)
 #     - Query to find all other registars
 
-
 import click
 import time
 
@@ -77,11 +76,11 @@ __all__ = []
 
 _LOGGER = aiko.logger(__name__)
 _PRIMARY_SEARCH_TIMEOUT = 2.0  # seconds
-
-services = {}
-time_started = time.time()
+_STATE_MACHINE = None
+_TIME_STARTED = time.time()
 
 # --------------------------------------------------------------------------- #
+
 
 class StateMachineModel(object):
     states = [
@@ -92,11 +91,16 @@ class StateMachineModel(object):
     ]
 
     transitions = [
-        {"source": "start", "trigger": "initialize", "dest": "primary_search"},
-        {"source": "primary_search", "trigger": "primary_found", "dest": "secondary"},
-        {"source": "primary_search", "trigger": "primary_promotion", "dest": "primary"},
-        {"source": "primary", "trigger": "primary_failed", "dest": "primary_search"},
-        {"source": "secondary", "trigger": "primary_failed", "dest": "primary_search"}
+        {"source":
+          "start", "trigger": "initialize", "dest": "primary_search"},
+        {"source":
+          "primary_search", "trigger": "primary_found", "dest": "secondary"},
+        {"source":
+          "primary_search", "trigger": "primary_promotion", "dest": "primary"},
+        {"source":
+          "primary", "trigger": "primary_failed", "dest": "primary_search"},
+        {"source":
+          "secondary", "trigger": "primary_failed", "dest": "primary_search"}
     ]
 
     def on_enter_primary_search(self, event_data):
@@ -108,11 +112,11 @@ class StateMachineModel(object):
         event.add_timer_handler(self.primary_search_timer, _PRIMARY_SEARCH_TIMEOUT)
 
     def primary_search_timer(self):
-        timer_valid = state_machine.get_state() == "primary_search"
+        timer_valid = _STATE_MACHINE.get_state() == "primary_search"
         _LOGGER.debug(f"timer primary_search {timer_valid}")
         event.remove_timer_handler(self.primary_search_timer)
         if timer_valid:
-            state_machine.transition("primary_promotion", None)
+            _STATE_MACHINE.transition("primary_promotion", None)
 
     def on_enter_secondary(self, event_data):
         _LOGGER.debug("do enter_secondary")
@@ -122,103 +126,137 @@ class StateMachineModel(object):
         # Clear LWT, so this registrar doesn't receive another LWT on reconnect
         aiko.public.message.publish(aiko.REGISTRAR_TOPIC, "", retain=True)
         lwt_payload = "(primary stopped)"
-        aiko.set_last_will_and_testament(aiko.REGISTRAR_TOPIC, lwt_payload, True)
-        payload_out = f"(primary started {aiko.public.topic_path} {time_started})"
-        aiko.public.message.publish(aiko.REGISTRAR_TOPIC, payload_out, retain=True)
-
-state_machine = StateMachine(StateMachineModel())
+        aiko.set_last_will_and_testament(
+            aiko.REGISTRAR_TOPIC, lwt_payload, True)
+        payload_out =  \
+            f"(primary started {aiko.public.topic_path} {_TIME_STARTED})"
+        aiko.public.message.publish(
+            aiko.REGISTRAR_TOPIC, payload_out, retain=True)
 
 # --------------------------------------------------------------------------- #
 
-def registrar_handler(aiko, action, registrar):
-    if action == "started":
-        if state_machine.get_state() == "primary_search":
-            state_machine.transition("primary_found", None)
+class Registrar(Service):
+    Interface.implementations["Registrar"] = "aiko_services.registrar.RegistrarImpl"
 
-    if action == "stopped":
-        if state_machine.get_state() == "primary_search":
-            state_machine.transition("primary_promotion", None)
-        else:
-            services = {}
-            state_machine.transition("primary_failed", None)
+class RegistrarImpl(Registrar):
+    def __init__(self, implementations):
+        implementations["Service"].__init__(self, implementations)
+        aiko.set_protocol(aiko.REGISTRAR_PROTOCOL)  # TODO: Move into service.py
 
-    return False
+        self.services = {}
 
-def service_state_handler(aiko, topic, payload_in):
-    command, parameters = parse(payload_in)
-    if command == "stopped" and topic.endswith("/state"):
-        topic_path = topic[:-len("/state")]
-        service_remove(topic_path)
-        payload_out = f"(remove {topic_path})"
-        aiko.message.publish(aiko.topic_out, payload_out)
-
-def topic_in_handler(aiko, topic, payload_in):
-    command, parameters = parse(payload_in)
-#   _LOGGER.debug(f"topic_in_handler(): {command}: {parameters}")
-
-    if len(parameters) > 0:
-        topic_path = parameters[0]
-        if len(parameters) == 5:
-            protocol = parameters[1]
-            transport = parameters[2]
-            owner = parameters[3]
-            tags = parameters[4]
-
-    if command == "add" and len(parameters) == 5:
-        service_add(topic_path, protocol, transport, owner, tags)
-        payload_out = payload_in
-        aiko.message.publish(aiko.topic_out, payload_out)
-
-    if command == "remove" and len(parameters) == 1:
-        service_remove(topic_path)
-        payload_out = payload_in
-        aiko.message.publish(aiko.topic_out, payload_out)
-
-    if command == "query" and len(parameters) == 5:
-        filter = ServiceFilter("*", protocol, transport, owner, tags)
-        services_out = filter_services_by_attributes(services, filter)
-
-        payload_out = f"(item_count {len(services_out)})"
-        aiko.message.publish(topic_path, payload=payload_out)
-
-        for service_topic, service_details in services_out.items():
-            service_tags = " ".join(service_details["tags"])
-            payload_out =  "(add"                             \
-                          f" {service_topic}"                 \
-                          f" {service_details['protocol']}"   \
-                          f" {service_details['transport']}"  \
-                          f" {service_details['owner']}"      \
-                          f" ({service_tags}))"
-            aiko.message.publish(topic_path, payload_out)
-
-        payload_out = "(sync " + topic_path + ")"
-        aiko.message.publish(aiko.topic_out, payload_out)
-
-def service_add(service_topic, protocol, transport, owner, tags):
-    if service_topic not in services:
-        _LOGGER.debug(f"Service add: {service_topic}")
-        service_details = {
-            "protocol": protocol,
-            "transport": transport,
-            "owner": owner,
-            "tags": tags
+        self.state = {
+            "lifecycle": "ready",
+            "log_level": get_log_level_name(_LOGGER),
+            "service_count": 0,
+            "source_file": __file__
         }
-        services[service_topic] = service_details
+        self.ec_producer = ECProducer(self.state)
+        self.ec_producer.add_handler(self._ec_producer_change_handler)
 
-def service_remove(service_topic):
-    if service_topic in services:
-        _LOGGER.debug(f"Service remove: {service_topic}")
-        del services[service_topic]
+        aiko.set_registrar_handler(self._registrar_handler)
+        aiko.add_message_handler(
+            self._service_state_handler, aiko.SERVICE_STATE_TOPIC)
+        aiko.add_topic_in_handler(self._topic_in_handler)
+
+        _STATE_MACHINE.transition("initialize", None)
+
+    def _ec_producer_change_handler(self, command, item_name, item_value):
+        if item_name == "log_level":
+            _LOGGER.setLevel(str(item_value).upper())
+
+    def _registrar_handler(self, aiko, action, registrar):
+        if action == "started":
+            if _STATE_MACHINE.get_state() == "primary_search":
+                _STATE_MACHINE.transition("primary_found", None)
+
+        if action == "stopped":
+            if _STATE_MACHINE.get_state() == "primary_search":
+                _STATE_MACHINE.transition("primary_promotion", None)
+            else:
+                self.services = {}
+                _STATE_MACHINE.transition("primary_failed", None)
+
+        return False
+
+    def _service_state_handler(self, aiko, topic, payload_in):
+        command, parameters = parse(payload_in)
+        if command == "stopped" and topic.endswith("/state"):
+            topic_path = topic[:-len("/state")]
+            self._service_remove(topic_path)
+            payload_out = f"(remove {topic_path})"
+            aiko.message.publish(aiko.topic_out, payload_out)
+
+    def _topic_in_handler(self, aiko, topic, payload_in):
+        command, parameters = parse(payload_in)
+        _LOGGER.debug(f"topic_in_handler(): {command}: {parameters}")
+
+        if len(parameters) > 0:
+            topic_path = parameters[0]
+            if len(parameters) == 5:
+                protocol = parameters[1]
+                transport = parameters[2]
+                owner = parameters[3]
+                tags = parameters[4]
+
+        if command == "add" and len(parameters) == 5:
+            self._service_add(topic_path, protocol, transport, owner, tags)
+            payload_out = payload_in
+            aiko.message.publish(aiko.topic_out, payload_out)
+
+        if command == "remove" and len(parameters) == 1:
+            self._service_remove(topic_path)
+            payload_out = payload_in
+            aiko.message.publish(aiko.topic_out, payload_out)
+
+        if command == "query" and len(parameters) == 5:
+            filter = ServiceFilter("*", protocol, transport, owner, tags)
+            services_out = filter_services_by_attributes(self.services, filter)
+
+            payload_out = f"(item_count {len(services_out)})"
+            aiko.message.publish(topic_path, payload=payload_out)
+
+            for service_topic, service_details in services_out.items():
+                service_tags = " ".join(service_details["tags"])
+                payload_out =  "(add"                             \
+                              f" {service_topic}"                 \
+                              f" {service_details['protocol']}"   \
+                              f" {service_details['transport']}"  \
+                              f" {service_details['owner']}"      \
+                              f" ({service_tags}))"
+                aiko.message.publish(topic_path, payload_out)
+
+            payload_out = "(sync " + topic_path + ")"
+            aiko.message.publish(aiko.topic_out, payload_out)
+
+    def _service_add(self, service_topic, protocol, transport, owner, tags):
+        if service_topic not in self.services:
+            _LOGGER.debug(f"Service add: {service_topic}")
+            service_details = {
+                "protocol": protocol,
+                "transport": transport,
+                "owner": owner,
+                "tags": tags
+            }
+            self.services[service_topic] = service_details
+            service_count = self.state["service_count"] + 1
+            self.ec_producer.update("service_count", service_count)
+
+    def _service_remove(self, service_topic):
+        if service_topic in self.services:
+            _LOGGER.debug(f"Service remove: {service_topic}")
+            del self.services[service_topic]
+            service_count = self.state["service_count"] - 1
+            self.ec_producer.update("service_count", service_count)
 
 # --------------------------------------------------------------------------- #
 
-@click.command()
+@click.command("main", help="Registrar Service")
 def main():
-    aiko.set_protocol(aiko.REGISTRAR_PROTOCOL)  # TODO: Move into service.py
-    aiko.add_message_handler(service_state_handler, aiko.SERVICE_STATE_TOPIC)
-    aiko.set_registrar_handler(registrar_handler)
-    aiko.add_topic_in_handler(topic_in_handler)
-    state_machine.transition("initialize", None)
+    global _STATE_MACHINE
+    _STATE_MACHINE = StateMachine(StateMachineModel())
+
+    registrar = compose_instance(RegistrarImpl, {})
     aiko.process(True)
 
 if __name__ == "__main__":
