2e28d1f627d4de454b69a144f9a4f24218be3223
Andy Gelme <geekscape@gmail.com>
2022-11-10 16:35:09 +1100

    Add "aiko_services/process.py" and "aiko_services/utilities/lock.py".

---
+++ aiko_services/process.py
@@ -0,0 +1,321 @@
+# Description
+# ~~~~~~~~~~~
+# Provides the AikoServices framework for an application process.
+# Supports none, one or many Services within a single Process.
+#
+# Usage
+# ~~~~~
+# def time_handler():
+#     print("timer_handler()")
+#
+# from aiko_services import *            # Invokes __init__.py
+# aiko.process = process_create()        # Invoked from __init__.py
+# event.add_timer_handler(handler, 1.0)
+# aiko.process.run()                     # Invoked by application code
+#
+# To Do
+# ~~~~~
+# - A Process with ...
+#   - Single Service:    Typically isn't a LifeCycleManager
+#   - Multiple Services: First Service is a LifeCycleManager or Pipeline
+#     - Use optimized single Process LifeCycleManager (not distributed) ?
+#     - Using local or remote functional calls should be very similar ?
+#
+# - Ensure that add_service() and remove_service() update the Registrar
+# - Consider moving all registrar related functions into the Registar
+#
+# - If a Service event (message, timer) handler raises an Exception,
+#   then provide the option to terminate and remove the Service or
+#   terminate the entire Process
+#
+# - Replace global Event functions with a Handler class instance
+#
+# - Improve AikoLogger (console and MQTT) to accurately provide Service id
+#   - Use ContextManager ?
+#
+# - Automatically attempt reconnection with MQTT server after disconnection
+#
+# - Implement multiple message server connections
+#
+# - Review "event.py" message and mailbox queues for latency and bandwidth
+#   - Minimal latency from MQTT on_message() handler --> application handler
+#   - Minimal latency for priority message handling, whilst handling others
+# - Load test 1 Process containing 1,000+ Services
+# - Load test 1 Process containing 10,000+ Services (what is the limit ?)
+# - Load test 1,000+ Process containing 1,000+ Services
+# - Load test 1,000+ Process containing 10,000+ Services (what is the limit ?)
+#
+# - HL Fleet
+#   - HL Serving instances: Provide Ray head and worker node public IP addresses
+#   - Registrar History and Recorder --> Dashboard, local and remote storage ?
+#   - HostService: monitor CPU, memory, disk, GPU, network, mosquitto, etc
+#     - Run on artemis, zeus, AWS EC2 Ray head and worker nodes
+#   - Statistics: InfluxDB --> Grafana
+
+import os
+import sys
+import traceback
+
+from aiko_services import *
+from aiko_services.message import *
+from aiko_services.utilities import *
+
+__all__ = ["aiko", "process_create"]
+
+_VERSION = 0
+
+# Environment variable names
+_AIKO_LOG_LEVEL_MESSAGE = "AIKO_LOG_LEVEL_MESSAGE"
+_AIKO_LOG_LEVEL_PROCESS = "AIKO_LOG_LEVEL_PROCESS"
+
+# --------------------------------------------------------------------------- #
+# ProcessData is intended as a singleton class data structure only
+
+class ProcessData:
+    TOPIC_REGISTRAR_BOOT = f"{get_namespace()}/service/registrar"
+
+    connection = Connection()
+#   event = aiko_services.event  # TODO: Replace with Handler() instance
+    logger = None
+    message = None
+    process = None
+    registrar = None
+
+    topic_path_process = f"{get_namespace()}/{get_hostname()}/{get_pid()}"
+    topic_path = f"{topic_path_process}/0"
+    topic_log = f"{topic_path}/log"
+    topic_lwt = f"{topic_path}/state"
+    payload_lwt = "(absent)"
+
+    @classmethod
+    def get_topic_path(cls, service_id):
+        return f"{cls.topic_path_process}/{service_id}"
+
+aiko = ProcessData
+
+# --------------------------------------------------------------------------- #
+# AikoLogger should be usable even before ProcessImplementation() is created
+
+class AikoLogger:
+    @classmethod
+    def logger(cls,
+        name, log_level=None, logging_handler=None, topic=aiko.topic_log):
+
+        if logging_handler is None:
+            if os.environ.get("AIKO_LOG_MQTT", "true") == "true":
+                logging_handler = LoggingHandlerMQTT(aiko, topic)
+        return get_logger(name, log_level, logging_handler)
+
+aiko.logger = AikoLogger.logger
+
+_LOGGER_MESSAGE = aiko.logger(
+    f"{__name__}.message",
+    log_level=os.environ.get(_AIKO_LOG_LEVEL_MESSAGE, "INFO")
+)
+
+_LOGGER = aiko.logger(
+    __name__, log_level=os.environ.get(_AIKO_LOG_LEVEL_PROCESS, "INFO")
+)
+
+# --------------------------------------------------------------------------- #
+# ProcessImplementation is intended as a singleton class implementation only
+
+class ProcessImplementation(ProcessData):
+    def __init__(self):
+        self.initialized = False
+        self.running = False  # TODO: Replace with a StateMachine
+        self.service_count = 0
+
+        self._exit_status = 0
+        self._message_handlers = {}
+        self._message_handlers_wildcard_topics = []
+        self._registrar_absent_terminate = False
+        self._services = {}
+        self._services_lock = Lock(f"{__name__}._services", _LOGGER)
+
+    def initialize(self):
+        if not self.initialized:
+            self.initialized = True
+            event.add_queue_handler(self.on_message_queue_handler, ["message"])
+            self.add_message_handler(
+                self.on_registrar, aiko.TOPIC_REGISTRAR_BOOT)
+            aiko.message = MQTT(
+                self.on_message, self._message_handlers,
+                aiko.topic_lwt, aiko.payload_lwt, False
+            )
+            context = ContextManager(aiko, aiko.message)
+
+    def run(self, loop_when_no_handlers=False):
+        self.initialize()
+
+        if not self.running:
+            self.running = True
+            event.loop(loop_when_no_handlers)  ## Blocking call ##
+            self.running = False
+
+        if self._exit_status:
+            sys.exit(self._exit_status)
+
+    def add_message_handler(self, message_handler, topic):
+        if not topic in self._message_handlers:
+            self._message_handlers[topic] = []
+            if ("#" in topic) or ("+" in topic):
+                self._message_handlers_wildcard_topics.append(topic)
+            if aiko.message:
+                aiko.message.subscribe(topic)
+        self._message_handlers[topic].append(message_handler)
+
+    def remove_message_handler(self, message_handler, topic):
+        if topic in self._message_handlers:
+            if message_handler in self._message_handlers[topic]:
+                self._message_handlers[topic].remove(message_handler)
+            if len(self._message_handlers[topic]) == 0:
+                del self._message_handlers[topic]
+                if topic in self._message_handlers_wildcard_topics:
+                    del self._message_handlers_wildcard_topics[topic]
+                if self.message:
+                    self.message.unsubscribe(topic)
+
+    def _add_service_to_registrar(self, service):
+        if service.protocol:
+            owner = get_username()
+            tags = service.get_tags_string()
+            # TODO: For payload_out, use parser.generate() ?
+            payload_out = f"(add {service.topic_path} {service.protocol} "  \
+                          f"{service.transport} {owner} ({tags}))"
+            registrar_topic_in = f"{aiko.registrar['topic_path']}/in"
+            aiko.message.publish(registrar_topic_in, payload_out)
+
+    def _remove_service_from_registrar(self, service):
+        if service.protocol:
+            # TODO: For payload_out, use parser.generate() ?
+            payload_out = f"(remove {service.topic_path})"
+            registrar_topic_in = f"{aiko.registrar['topic_path']}/in"
+            aiko.message.publish(registrar_topic_in, payload_out)
+
+    def add_service(self, service):
+        self._services_lock.acquire("add_service()")
+        self.service_count += 1
+        service.service_id = self.service_count
+        service.topic_path = aiko.get_topic_path(service.service_id)
+        self._services[service.service_id] = service
+        self._services_lock.release()
+        if self.connection.is_connected(ConnectionState.REGISTRAR):
+            self._add_service_to_registrar(service)
+        return self.service_count
+
+    def remove_service(self, service_id):
+        self._services_lock.acquire("remove_service")
+        if service_id in self._services:
+            del self._services[service_id]
+            self.service_count -= 1
+        self._services_lock.release()
+        if self.connection.is_connected(ConnectionState.REGISTRAR):
+            self._remove_service_from_registrar(service)
+        return self.service_count
+
+    def on_message(self, mqtt_client, userdata, message):
+        try:
+            event.queue_put(message, "message")
+        except Exception as exception:
+            print(traceback.format_exc())
+
+    def on_message_queue_handler(self, message, _):
+        topic = message.topic
+        payload_in = message.payload.decode("utf-8")
+        if _LOGGER_MESSAGE.isEnabledFor(DEBUG):  # Save time
+            _LOGGER_MESSAGE.debug(f"Message: {topic}: {payload_in}")
+
+        message_handler_list = []
+        topics_matched = self.topic_matcher(topic, self._message_handlers)
+        for topic_match in topics_matched:
+            message_handler_list.extend(self._message_handlers[topic_match])
+
+        if len(message_handler_list) > 0:
+            for message_handler in message_handler_list:
+                try:
+                    if message_handler(aiko, topic, payload_in):
+                        return
+                except Exception as exception:  # REVIEW
+                    payload_out = traceback.format_exc()
+                    print(payload_out)
+                    aiko.message.publish(aiko.topic_log, payload_out)
+
+    def on_registrar(self, _, topic, payload_in):
+        command, parameters = parse(payload_in)
+        action = None
+        registrar = {}
+        parse_okay = False
+        if len(parameters) > 0:
+            action = parameters[0]
+            if command == "primary":
+                if len(parameters) == 3 and action == "found":
+                    registrar["topic_path"] = parameters[1]
+                    registrar["timestamp"] = parameters[2]
+                    parse_okay = True
+                if len(parameters) == 1 and action == "absent":
+                    parse_okay = True
+
+        if parse_okay:
+            if action == "found":
+                aiko.registrar = registrar
+                aiko.connection.update_state(ConnectionState.REGISTRAR)
+
+                self._services_lock.acquire("on_registrar() #1")
+                for service in self._services.values():
+                    self._add_service_to_registrar(service)
+                self._services_lock.release()
+
+            if action == "absent":
+                aiko.registrar = None
+                aiko.connection.update_state(ConnectionState.TRANSPORT)
+                if self._registrar_absent_terminate:
+                    self.terminate(1)
+
+            self._services_lock.acquire("on_registrar() #2")
+            for service in self._services.values():
+                service.registrar_handler_call(action, aiko.registrar)
+            self._services_lock.release()
+
+    def set_last_will_and_testament(self,
+        topic_lwt, payload_lwt="(absent)", retain_lwt=False):
+
+        aiko.message.set_last_will_and_testament(
+            topic_lwt, payload_lwt, retain_lwt)
+
+    def set_registrar_absent_terminate(self):
+        self._registrar_absent_terminate = True
+
+    def terminate(self, exit_status=0):
+        self._exit_status = exit_status
+        event.terminate()
+
+# topic_matcher() matches a ...
+# - literal topic without wildcards
+# - topic ending with a "#" wildcard, e.g prefix/#
+# - topic containing "+" wildcards, e.g prefix/+/+/+/suffix
+
+    def topic_matcher(self, topic, topics):
+        if topic in topics:
+            topics_matched = [topic]
+        else:
+            topics_matched = []
+
+        for wildcard_topic in self._message_handlers_wildcard_topics:
+            tokens = topic.split("/")
+            wildcard_tokens = wildcard_topic.split("/")
+
+            if wildcard_tokens[-1] == "#":
+                if tokens[:-1] == wildcard_tokens[:-1]:
+                    topics_matched.append(wildcard_topic)
+            elif tokens[0] == wildcard_tokens[0] and  \
+                 tokens[-1] == wildcard_tokens[-1]:
+                    topics_matched.append(wildcard_topic)
+        return topics_matched
+
+def process_create():
+    if not ProcessData.process:
+        ProcessData.process = ProcessImplementation()
+    return ProcessData.process
+
+# --------------------------------------------------------------------------- #
---
+++ aiko_services/utilities/lock.py
@@ -0,0 +1,33 @@
+# Usage
+# ~~~~~
+# from aiko_services.utilities import *
+# lock_test = Lock(f"{__name__}.test", aiko.logger(__name__))
+# lock_test.acquire("add_service()")
+### Critial code
+# lock_test.release()
+
+from threading import Lock as ThreadingLock
+
+__all__ = ["Lock"]
+
+class Lock:
+    def __init__(self, name, logger=None):
+        self._name = name
+        self._logger = logger
+        self._lock = ThreadingLock()
+        self._in_use = None
+
+    def acquire(self, location):
+        if self._in_use:
+            diagnostic = f'Lock "{self._name}" '  \
+                         f'at "{location}" in use by "{self._in_use}"'
+            if self._logger:
+                self._logger.warn(diagnostic)
+            else:
+                print(diagnostic)
+        self._lock.acquire()
+        self._in_use = location
+
+    def release(self):
+        self._in_use = None
+        self._lock.release()
