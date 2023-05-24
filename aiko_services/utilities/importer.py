eceabe591a321b017bc02a747e6ab50c80a164af
Andy Gelme <geekscape@gmail.com>
2023-05-24 18:06:38 +1000

    Fix "aiko_services/utilities/importer.py" to only load a specified module once.

--- aiko_services/utilities/importer.py
+++ aiko_services/utilities/importer.py
@@ -19,15 +19,22 @@ __all__ = ["load_module", "load_modules"]
 if os.environ.get("AIKO_IMPORTER_USE_CURRENT_DIRECTORY"):
     sys.path.append(os.getcwd())
 
+MODULES_LOADED = {}
+
 def load_module(module_descriptor):
-    if module_descriptor.endswith(".py"):
-        # Load module from Python source pathname, e.g "directory/file.py"
-        module_pathname = module_descriptor
-        module = importlib.machinery.SourceFileLoader('module', module_pathname).load_module()
+    if module_descriptor in MODULES_LOADED:
+        module = MODULES_LOADED[module_descriptor]
     else:
-        # Load module from "installed" modules, e.g "package.module"
-        module_name = module_descriptor
-        module = importlib.import_module(module_name)
+        if module_descriptor.endswith(".py"):
+            # Load module from Python source pathname, e.g "directory/file.py"
+            module_pathname = module_descriptor
+            module = importlib.machinery.SourceFileLoader(
+                'module', module_pathname).load_module()
+        else:
+            # Load module from "installed" modules, e.g "package.module"
+            module_name = module_descriptor
+            module = importlib.import_module(module_name)
+        MODULES_LOADED[module_descriptor] = module
     return module
 
 def load_modules(module_pathnames):
