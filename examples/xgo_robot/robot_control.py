ebe5ce1018c664ab1e0cd0a070703c91be8cd41f
Andy Gelme <geekscape@gmail.com>
2023-06-13 07:44:04 +1000

    Improve XGO-Mini 2 "robot_control.py" for CLI argument "topic_path" and being able to send commands to the robot.  For example "reset" and toggle "screen_detail".

--- examples/xgo_robot/robot_control.py
+++ examples/xgo_robot/robot_control.py
@@ -2,22 +2,48 @@
 #
 # Usage
 # ~~~~~
-# ./robot_control.py ui          # Display video and provide keyboard control
-# ./robot_control.py video_test  # Test for video transmission and display
+# ./robot_control.py ui [topic_path]  # Control robot and display camera input
+# ./robot_control.py video_test       # Test for video transmission and display
 #
 # UI keyboard commands
+# - r: Reset robot actuators to initial positions
 # - s: Save image to disk as "z_image_??????.jpg"
-# - x: Exit
+# - v: Verbose screen detail (toggle state)
+# - x: eXit
+#
+# Refactor
+# ~~~~~~~~
+# - ServiceDiscovery / ActorDiscovery (pipeline.py, transport/)
+# - Combine _image_to_bgr() and _image_to_rgb() --> _image_convert()
+# - Move to "aiko_services/video/" (or "media/") ...
+#  - _camera*() and _screen*() functions
+#  - _image_convert(), _image_resize()
+# - Generalize daggy video (and audio) compression / transmission over MQTT
+# - Generalize image overlay
+# - Resource monitoring: CPU, memory, network, etc
 #
 # To Do
 # ~~~~~
-# - Payload includes frame id and other status, e.g battery level, motor speed
-# - Keyboard commands ...
-#   - Save received image to disk
+# - Discover "xgo_robot" Actor and use "topic_path/video" instead
+#   - When absent, display a video image that shows "robot not found"
+#
+# - Keyboard commands: Display "help" on screen, key='?' ...
+#   - Reset (R)
+#   - Terminate (T) and Halt/Shutdown (H)
+#   - Action, e.g sit
+#   - Arm mode ... and arm and claw position
+#   - Pitch, roll, yaw: keyboard arrows and display value
+#   - Translate: x, y, z: keyboard arrows and display value
 #   - Move forward, back, left, right, turn
-#   - Motor speed
-#   - Pitch, roll, yaw
-#   - Arm and claw position
+#   - Motor speed and display value
+#   - Pick-up and put-down ball
+#
+# - Payload includes frame id and other status, e.g battery level, motor speed
+#
+# - Send text to display on video screen
+# - Send video to the LCD screen (overlay with local status)
+# - Send audio sample (MP3 ?) to speaker
+# - Disable / enable microphone audio transmission
 
 from abc import abstractmethod
 import click
@@ -49,11 +75,15 @@ class RobotControl(Actor):
         pass
 
 class RobotControlImpl(RobotControl):
-    def __init__(self, implementations, name, protocol, tags, transport):
+    def __init__(self,
+        implementations, name, protocol, tags, transport,
+        robot_topic):
+
         implementations["Actor"].__init__(self,
             implementations, name, protocol, tags, transport)
 
         self.state["frame_id"] = 0
+        self.state["robot_topic"] = robot_topic
         self.state["source_file"] = f"v{_VERSION}⇒{__file__}"
         self.state["topic_video"] = TOPIC_VIDEO
 
@@ -71,16 +101,36 @@ class RobotControlImpl(RobotControl):
         image = np.load(payload_in, allow_pickle=True)
 
         image = self._image_to_bgr(image)
-        cv2.imshow("xgo_robot", image)
+        cv2.imshow("xgo_robot", self._image_resize(image))
         key = cv2.waitKey(1) & 0xff
+        print(key)
+        if key == ord("r"):
+            payload_out = "(reset)"
+            aiko.message.publish(
+                f"{self.state['robot_topic']}/in", payload_out)
+            payload_out = "(claw 0)"
+            aiko.message.publish(
+                f"{self.state['robot_topic']}/in", payload_out)
         if key == ord("s"):
             cv2.imwrite(f"z_image_{frame_id:06d}.jpg", image)
+        if key == ord("v"):
+            payload_out = "(screen_detail)"  # toggle state
+            aiko.message.publish(
+                f"{self.state['robot_topic']}/in", payload_out)
         if key == ord("x"):
+            cv2.destroyAllWindows()
             raise SystemExit()
 
     def _image_to_bgr(self, image):
         r, g, b = cv2.split(image)
         image = cv2.merge((b, g, r))
+        return image
+
+    def _image_resize(self, image, scale=2):
+        width = int(image.shape[1] * scale)
+        height = int(image.shape[0] * scale)
+        dimensions = (width, height)
+        image = cv2.resize(image, dimensions, interpolation = cv2.INTER_AREA)
         return image
 
 # --------------------------------------------------------------------------- #
@@ -153,8 +203,10 @@ def main():
     pass
 
 @main.command(help="Robot Control user interface")
-def ui():
+@click.argument("robot_topic", default=None, required=False)
+def ui(robot_topic):
     init_args = actor_args(ACTOR_TYPE_UI, PROTOCOL_UI)
+    init_args["robot_topic"] = robot_topic
     robot_control = compose_instance(RobotControlImpl, init_args)
     aiko.process.run()
 
