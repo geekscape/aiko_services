3653149196b47ed909837da525d7dd6c483618b1
Andy Gelme <geekscape@gmail.com>
2023-06-30 11:29:22 +1000

    Add simple frame generator PipelineElement example to "aiko_services/pipeline_elements.py".

--- aiko_services/pipeline_elements.py
+++ aiko_services/pipeline_elements.py
@@ -6,12 +6,48 @@
 #      tr -s " " | cut -d" " -f1`;  \
 #      mosquitto_pub -h localhost -t aiko/spike/$PID/1/in  \
 #                    -m "(process_frame (stream_id: 0 frame_id: 0) (a: 0))"
+#
+# mosquitto_pub -h localhost -t aiko/spike/$PID/1/in -m "(create_stream 1)"
+# mosquitto_pub -h localhost -t aiko/spike/$PID/1/in -m "(destroy_stream 1)"
 
+from threading import Thread
+import time
 from typing import Tuple
 
 from aiko_services import aiko, PipelineElement
 
 _LOGGER = aiko.logger(__name__)
+
+# --------------------------------------------------------------------------- #
+# TODO: Replace thread with "event.add_timer_handler()"
+
+class PE_GenerateNumbers(PipelineElement):
+    def __init__(self,
+        implementations, name, protocol, tags, transport,
+        definition, pipeline):
+
+        implementations["PipelineElement"].__init__(self,
+            implementations, name, protocol, tags, transport,
+            definition, pipeline)
+
+    def process_frame(self, context, number) -> Tuple[bool, dict]:
+        _LOGGER.debug(f"{self._id(context)}: in/out number: {number}")
+        return True, {"number": number}
+
+    def _run(self, context):
+        while not context["terminate"]:
+            context["frame_id"] += 1
+            self.pipeline.create_frame(context, {"number": context["frame_id"]})
+            time.sleep(1.0)
+
+    def start_stream(self, context, stream_id):
+        context["terminate"] = False
+        Thread(target=self._run, args=(context, )).start()
+
+    def stop_stream(self, context, stream_id):
+        context["terminate"] = True
+
+# --------------------------------------------------------------------------- #
 
 class PE_0(PipelineElement):
     def __init__(self,
@@ -28,6 +64,8 @@ class PE_0(PipelineElement):
         _LOGGER.info(f"PE_0: {context}, in a: {a}, out b: {b}")
         return True, {"b": b}
 
+# --------------------------------------------------------------------------- #
+
 class PE_1(PipelineElement):
     def __init__(self,
         implementations, name, protocol, tags, transport,
@@ -42,6 +80,8 @@ class PE_1(PipelineElement):
         c = int(b) + 1
         _LOGGER.info(f"PE_1: {context}, in b: {b}, out c: {c}")
         return True, {"c": c}
+
+# --------------------------------------------------------------------------- #
 
 class PE_2(PipelineElement):
     def __init__(self,
@@ -58,6 +98,8 @@ class PE_2(PipelineElement):
         _LOGGER.info(f"PE_2: {context}, in c: {c}, out d: {d}")
         return True, {"d": d}
 
+# --------------------------------------------------------------------------- #
+
 class PE_3(PipelineElement):
     def __init__(self,
         implementations, name, protocol, tags, transport,
@@ -73,6 +115,8 @@ class PE_3(PipelineElement):
         _LOGGER.info(f"PE_2: {context}, in c: {c}, out e: {e}")
         return True, {"e": e}
 
+# --------------------------------------------------------------------------- #
+
 class PE_4(PipelineElement):
     def __init__(self,
         implementations, name, protocol, tags, transport,
@@ -87,3 +131,5 @@ class PE_4(PipelineElement):
         f = int(d) + int(e)
         _LOGGER.info(f"PE_3: {context}, in d, e {d} {e}, out: d + e = f: {f}")
         return True, {"f": f}
+
+# --------------------------------------------------------------------------- #
