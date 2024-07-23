ebfb45e9b01e63f48fec0e84c10a78ecc2ff20ec
Andy Gelme <geekscape@gmail.com>
2024-07-23 19:05:31 +1000

    Fix "src/aiko_services/examples/pipeline/multitude/*.sh" scripts to use correct MQTT host names.

--- src/aiko_services/examples/pipeline/multitude/run_large.sh
+++ src/aiko_services/examples/pipeline/multitude/run_large.sh
@@ -21,56 +21,62 @@
 # - Suport multiple concurrent Streams
 # - Support unlimited number of Pipelines (using looping to start them)
 
+# export AIKO_MQTT_HOST=localhost
+
 export AIKO_LOG_LEVEL=${1:-WARNING}
 export AIKO_LOG_MQTT=${2:-true}
-export FRAME_DELAY=${3:-1.0}      # 1.0 / frame_rate
+FRAME_DELAY=${3:-1.0}                # 1.0 / frame_rate
 
-export USE_PIPELINE=000  # or 010, 020, ... 090
+USE_PIPELINE=000  # or 010, 020, ... 090
 
-export GRACE_TIME=10
-export PIPELINE_LIMIT=9
-export STREAM_ID=1
+GRACE_TIME=10
+PIPELINE_LIMIT=9
+STREAM_ID=1
 
-export process_frame=true
+process_frame=true
 
 trap ctrl_c_0 INT
 
 function ctrl_c_0() {
   trap ctrl_c_1 INT
   echo "  Pipelines still running, frame generation stopped"
-  export process_frame=false
+  process_frame=false
 }
 
 function ctrl_c_1() {
   echo "  Exit"
-  pkill aiko_pipeline  # CAUTION: Indiscriminately kills Aiko Pipelines !
+  kill $process_ids
   exit 0
 }
 
+process_ids=""
 pipeline_id=0
 while [ $pipeline_id -le $PIPELINE_LIMIT ]; do
   pipeline_name=0${pipeline_id}0
   pipeline_file=pipeline_large_${pipeline_name}
   aiko_pipeline create ${pipeline_file}.json &
-	# aiko_pipeline create ${pipeline_file}.json 2>${pipeline_file}.log &
-  export topic_pipeline_${pipeline_name}=aiko/spike/$!/1/in
+# aiko_pipeline create ${pipeline_file}.json 2>${pipeline_file}.log &
+  process_id=$!
+  process_ids="$process_ids $process_id"
+  export topic_pipeline_${pipeline_name}=aiko/$HOSTNAME/$process_id/1/in
   ((pipeline_id++))
 done
 
-export use_topic_name=topic_pipeline_${USE_PIPELINE}
-export use_topic=${!use_topic_name}
+use_topic_name=topic_pipeline_${USE_PIPELINE}
+use_topic=${!use_topic_name}
 
 # while true; do sleep 1.0; done  # stop for all Pipelines to start-up
 sleep 5.0  # wait for Pipelines to start-up
 
-mosquitto_pub -t $use_topic -m "(create_stream $STREAM_ID () $GRACE_TIME)"
+mosquitto_pub -h $AIKO_MQTT_HOST -t $use_topic -m  \
+  "(create_stream $STREAM_ID () $GRACE_TIME)"
 # sleep 2.0  # wait for Streams to be created
 
 frame_id=0
 while true; do
   if [[ $process_frame == true ]]; then
     echo Frame: $frame_id
-    mosquitto_pub -t $use_topic -m  \
+    mosquitto_pub -h $AIKO_MQTT_HOST -t $use_topic -m  \
       "(process_frame (stream_id: $STREAM_ID frame_id: $frame_id) (i: 0))"
     ((frame_id++))
   fi
--- src/aiko_services/examples/pipeline/multitude/run_small.sh
+++ src/aiko_services/examples/pipeline/multitude/run_small.sh
@@ -21,52 +21,58 @@
 # - Suport multiple concurrent Streams
 # - Support unlimited number of Pipelines (using looping to start them)
 
+# export AIKO_MQTT_HOST=localhost
+
 export AIKO_LOG_LEVEL=${1:-WARNING}
 export AIKO_LOG_MQTT=${2:-true}
-export FRAME_DELAY=${3:-1.0}      # 1.0 / frame_rate
+FRAME_DELAY=${3:-1.0}                # 1.0 / frame_rate
 
-export USE_PIPELINE=a  # or b, c
+USE_PIPELINE=a  # or b, c
 
-export GRACE_TIME=10
-export STREAM_ID=1
+GRACE_TIME=10
+STREAM_ID=1
 
-export process_frame=true
+process_frame=true
 
 trap ctrl_c_0 INT
 
 function ctrl_c_0() {
   trap ctrl_c_1 INT
   echo "  Pipelines still running, frame generation stopped"
-  export process_frame=false
+  process_frame=false
 }
 
 function ctrl_c_1() {
   echo "  Exit"
-  pkill aiko_pipeline  # CAUTION: Indiscriminately kills Aiko Pipelines !
+  kill $process_ids
   exit 0
 }
 
+process_ids=""
 for pipeline_id in a b c; do
   pipeline_name=pipeline_small_${pipeline_id}
   aiko_pipeline create ${pipeline_name}.json &
   # aiko_pipeline create ${pipeline_name}.json 2>${pipeline_name}.log &
-  export topic_pipeline_${pipeline_id}=aiko/spike/$!/1/in
+  process_id=$!
+  process_ids="$process_ids $process_id"
+  export topic_pipeline_${pipeline_id}=aiko/$HOSTNAME/$process_id/1/in
 done
 
-export use_topic_name=topic_pipeline_${USE_PIPELINE}
-export use_topic=${!use_topic_name}
+use_topic_name=topic_pipeline_${USE_PIPELINE}
+use_topic=${!use_topic_name}
 
 # while true; do sleep 1.0; done  # stop for all Pipelines to start-up
 sleep 1.0  # wait for Pipelines to start-up
 
-mosquitto_pub -t $use_topic -m "(create_stream $STREAM_ID () $GRACE_TIME)"
+mosquitto_pub -h $AIKO_MQTT_HOST -t $use_topic -m  \
+  "(create_stream $STREAM_ID () $GRACE_TIME)"
 # sleep 2.0  # wait for Streams to be created
 
 frame_id=0
 while true; do
   if [[ $process_frame == true ]]; then
     echo Frame: $frame_id
-    mosquitto_pub -t $use_topic -m  \
+    mosquitto_pub -h $AIKO_MQTT_HOST -t $use_topic -m  \
       "(process_frame (stream_id: $STREAM_ID frame_id: $frame_id) (i: 0))"
     ((frame_id++))
   fi
