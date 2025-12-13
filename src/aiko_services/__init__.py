c1c7765acfa6903cf79e653c56fe5a14a69f6b7f
Andy Gelme <geekscape@gmail.com>
2025-12-13 13:56:21 +1100

    Update "pyproject.toml" to support Python from 3.9.7 to 3.13.11.

--- src/aiko_services/__init__.py
+++ src/aiko_services/__init__.py
@@ -1,7 +1,7 @@
 import aiko_services.main
 
 __version__ = "v0.6"
-__id__ = "2025-12-13_a"
+__id__ = "2025-12-13_b"
 
 from aiko_services.main import *
 aiko.id = __id__        # aiko = main.process.ProcessData
