133c76e10a2a89b600a351fb53c777f0b81d833f
Andy Gelme <geekscape@gmail.com>
2026-07-25 17:05:56 +1000

    Update "src/aiko_services/__init__.py" to version and id "v0.8-dev 2026-07-25_a".

--- src/aiko_services/__init__.py
+++ src/aiko_services/__init__.py
@@ -1,7 +1,7 @@
 import aiko_services.main
 
-__version__ = "v0.7"
-__id__ = "2026-07-06_b"
+__version__ = "0.8-dev"
+__id__ = "2026-07-25_a"
 
 from aiko_services.main import *
 aiko.id = __id__        # aiko = main.process.ProcessData
